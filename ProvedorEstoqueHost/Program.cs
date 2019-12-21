cf8031ee197dcc1ac86ecce57fbd60d03ef36e3c
Thiago Barbosa de Souza <tbsouza@outlook.com>
2019-12-21 10:44:38 -0300

    Add ProvedorEstoqueHost

---
+++ ProvedorEstoqueHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using ServicoEstoque;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvedorEstoqueHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost servicoEstoqueHost = new ServiceHost(typeof(EstoqueService));
+            servicoEstoqueHost.Open();
+
+            Console.WriteLine("Serviço Ativado");
+            Console.ReadLine();
+            Console.WriteLine("Desativando Serviço");
+
+            servicoEstoqueHost.Close();
+        }
+    }
+}
